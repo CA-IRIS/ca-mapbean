1c0d8616ef380d384f99f5546646fbe717a79852
Tim Johnson (timothy.a.johnson@dot.state.mn.us) <Tim Johnson (timothy.a.johnson@dot.state.mn.us)>
2003-08-27 18:41:22 +0000

    [project @ 2003-08-27 13:41:22 by john3tim] added getIncidents method

--- src/main/us/mn/state/dot/map/IncidentLayer.java
+++ src/main/us/mn/state/dot/map/IncidentLayer.java
@@ -41,16 +41,16 @@ import us.mn.state.dot.shape.event.SelectMouseMode;
  * Displays incidents as icons on map.
  *
  * @author erik.engstrom@dot.state.mn.us
- * @version $Revision: 1.41 $ $Date: 2003/05/19 21:23:41 $
+ * @version $Revision: 1.42 $ $Date: 2003/08/27 13:41:22 $
  */
 public class IncidentLayer extends AbstractLayer implements
 		IncidentListener {
 
 	protected IncidentWrapper [] incidents = null;
-	
+
 	protected ListSelectionModel selectionModel =
 		new DefaultListSelectionModel();
-	
+
 	//private boolean directional = true;
 
     private boolean dirty = true;
@@ -69,8 +69,8 @@ public class IncidentLayer extends AbstractLayer implements
 		}
 	}
 
-    /** 
-     * Called by the incident client every 30 seconds, will only call for a 
+    /**
+     * Called by the incident client every 30 seconds, will only call for a
      * repaint when there is at least one active incident or the first time
      * there are no incidents (to clear the layer).
      */
@@ -105,20 +105,20 @@ public class IncidentLayer extends AbstractLayer implements
             dirty = false;
         }
 	}
-	
+
 	/**
 	 * Class for notifying gui of changes in the eventdispatch thread.
 	 */
 	private final class NotifyThread implements Runnable {
-		
+
 		private final IncidentLayer layer;
 		private final int type;
-		
+
 		public NotifyThread( IncidentLayer layer, int type ) {
 			this.layer = layer;
 			this.type = type;
 		}
-		
+
 		public void run() {
 			layer.notifyLayerChangedListeners( new LayerChangedEvent( layer,
 				type ) );
@@ -131,8 +131,8 @@ public class IncidentLayer extends AbstractLayer implements
 			renderer.render( g, selection[ i ] );
 		}
 	}
-	
-	
+
+
 	public void paint( Graphics2D g, LayerRenderer renderer ){
 		if ( incidents != null ) {
 			for ( int i = ( incidents.length - 1 ); i >= 0; i-- ){
@@ -156,7 +156,11 @@ public class IncidentLayer extends AbstractLayer implements
 		}
 		return result;
 	}
-	
+
+	public IncidentWrapper[] getIncidents() {
+			return incidents;
+	}
+
 	private MapObject getIncident( Point2D p ) {
 		MapObject result = null;
 		if ( incidents != null ) {
@@ -173,7 +177,7 @@ public class IncidentLayer extends AbstractLayer implements
 		}
 		return result;
 	}
-	
+
 	public final MapObject search( Rectangle2D searchArea,
 			LayerRenderer renderer ) {
 		MapObject result = null;
@@ -192,7 +196,7 @@ public class IncidentLayer extends AbstractLayer implements
 		}
 		return result;
 	}
-	
+
 	public MapObject search( Point2D p, LayerRenderer renderer ) {
 		MapObject result = null;
 		if ( incidents != null ) {
@@ -223,13 +227,13 @@ public class IncidentLayer extends AbstractLayer implements
 		});
 		return result;
 	}
-	
+
 	public class IncidentWrapper implements MapObject {
-		
+
 		private final Incident incident;
-		
+
 		private final Shape shape;
-		
+
 		public IncidentWrapper( Incident incident ) {
 			this.incident = incident;
 			GeneralPath path = new GeneralPath();
@@ -237,55 +241,55 @@ public class IncidentLayer extends AbstractLayer implements
 				( float ) incident.getY() );
 			shape = path;
 		}
-		
+
 		public Incident getIncident() {
 			return incident;
 		}
-		
+
 		public IncidentDescription getDescription() {
 			return incident.getDescription();
 		}
-		
+
 		public Shape getShape() {
 			return shape;
 		}
-		
+
 		public double getX() {
 			return incident.getX();
 		}
-		
+
 		public double getY() {
 			return incident.getY();
 		}
-		
+
 		public String toString() {
 			return incident.toString();
 		}
-		
+
 	}
-	
+
 	private class IncidentTheme extends Theme implements MapMouseListener {
-		
+
 		public IncidentTheme( Layer layer ) {
 			super( layer );
 			LayerRenderer renderer = new IncidentRenderer();
 			this.addLayerRenderer( renderer );
 			this.setCurrentLayerRenderer( renderer );
 		}
-		
+
 		public MapMouseListener getMapMouseListener() {
 			return this;
 		}
-		
+
 		public boolean mouseMoved( final java.awt.event.MouseEvent p1 ) {
 			return false;
 		}
-		
+
 		public java.lang.String[] getMouseModeServiceList() {
 			String[] result = { "Select" };
 			return result;
 		}
-		
+
 		public boolean listensToMouseMode( String modeName ) {
 			boolean result = false;
 			if ( modeName.equals( SelectMouseMode.MODE_ID ) ) {
@@ -293,28 +297,28 @@ public class IncidentLayer extends AbstractLayer implements
 			}
 			return result;
 		}
-		
+
 		public void mouseExited( final java.awt.event.MouseEvent event ) {
 		}
-		
+
 		public boolean mousePressed( final java.awt.event.MouseEvent event ) {
 			return false;
 		}
-		
+
 		public boolean mouseDragged( final java.awt.event.MouseEvent event ) {
 			return false;
 		}
-		
+
 		public void mouseMoved() {
 		}
-		
+
 		public void mouseEntered( final java.awt.event.MouseEvent event ) {
 		}
-		
+
 		public boolean mouseReleased( final java.awt.event.MouseEvent event ) {
 			return false;
 		}
-		
+
 		public boolean mouseClicked( final java.awt.event.MouseEvent event ) {
 			if ( ! this.isVisible() ) {
 				return false;
@@ -338,7 +342,7 @@ public class IncidentLayer extends AbstractLayer implements
 				return false;
 			} else {
 				MapObject[] oldSelections = getSelections();
-				if ( oldSelections.length > 0 && incident == 
+				if ( oldSelections.length > 0 && incident ==
 						oldSelections[ 0 ] ) {
 					clearSelections();
 				} else {
