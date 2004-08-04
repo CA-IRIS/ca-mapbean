58562f91ef5d1d0fd4a74bd7a291cfd861619a5f
Douglas Lau (doug.lau@dot.state.mn.us) <Douglas Lau (doug.lau@dot.state.mn.us)>
2004-08-04 19:03:53 +0000

    [project @ 2004-08-04 14:03:53 by lau1dou] Whitespace cleanup.

--- src/main/us/mn/state/dot/map/event/SelectMouseMode.java
+++ src/main/us/mn/state/dot/map/event/SelectMouseMode.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000  Minnesota Department of Transportation
+ * Copyright (C) 2000-2004  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,7 +16,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 package us.mn.state.dot.shape.event;
 
 import java.awt.event.MouseEvent;
@@ -24,88 +23,65 @@ import java.awt.event.MouseEvent;
 /**
  *
  * @author <a href="mailto:erik.engstrom@dot.state.mn.us">Erik Engstrom</a>
- * @version $Revision: 1.3 $ $Date: 2001/04/19 16:49:31 $
  */
 public class SelectMouseMode extends MouseModeAdapter {
-	
-	/**
-     * Mouse Mode identifier, which is "Select".
-     * This is returned on getID()
-     */
-    public final static transient String MODE_ID = "Select".intern();
 
-    /**
-	 * Creates new SelectMouseMode 
+	/**
+	 * Mouse Mode identifier, which is "Select".
+	 * This is returned on getID()
+	 */
+	static public final transient String MODE_ID = "Select".intern();
+
+	/**
+	 * Creates new SelectMouseMode
 	 * Default constructor.  Sets the ID to the modeID, and the
-     * consume mode to true. 
-     */
+	 * consume mode to true.
+	 */
 	public SelectMouseMode() {
-		this( true );
+		this(true);
 	}
-    
+
 	/**
-     * Construct a SelectMouseMode.
-     * The constructor that lets you set the consume mode. 
-     * @param consumeEvents the consume mode setting.
-     */
-    public SelectMouseMode( boolean consumeEvents ){
-		super( MODE_ID, consumeEvents );
-    }
+	 * Construct a SelectMouseMode.
+	 * The constructor that lets you set the consume mode.
+	 * @param consumeEvents the consume mode setting.
+	 */
+	public SelectMouseMode(boolean consumeEvents) {
+		super(MODE_ID, consumeEvents);
+	}
 
-    /**
-     * Fires the MapMouseSupport method.
-     * @param e mouse event.
-     */
-    public void mouseClicked( MouseEvent e ) {
-		mouseSupport.fireMapMouseClicked( e );
-    }
+	/** Fire the MapMouseSupport method */
+	public void mouseClicked(MouseEvent e) {
+		mouseSupport.fireMapMouseClicked(e);
+	}
 
-    /**
-     * Fires the MapMouseSupport method.
-     * @param e mouse event.
-     */
-    public void mousePressed( MouseEvent e ) {
-		mouseSupport.fireMapMousePressed( e );
-    }
+	/** Fire the MapMouseSupport method */
+	public void mousePressed(MouseEvent e) {
+		mouseSupport.fireMapMousePressed(e);
+	}
 
-    /**
-     * Fires the MapMouseSupport method.
-     * @param e mouse event.
-     */
-    public void mouseReleased( MouseEvent e ) {
-		mouseSupport.fireMapMouseReleased( e );
-    }
+	/** Fire the MapMouseSupport method */
+	public void mouseReleased(MouseEvent e) {
+		mouseSupport.fireMapMouseReleased(e);
+	}
 
-    /**
-     * Fires the MapMouseSupport method.
-     * @param e mouse event.
-     */
-    public void mouseEntered( MouseEvent e ) {
-		mouseSupport.fireMapMouseEntered( e );
-    }
+	/** Fire the MapMouseSupport method */
+	public void mouseEntered(MouseEvent e) {
+		mouseSupport.fireMapMouseEntered(e);
+	}
 
-    /**
-     * Fires the MapMouseSupport method.
-     * @param e mouse event.
-     */
-    public void mouseExited( MouseEvent e ) {
-		mouseSupport.fireMapMouseExited( e );
-    }
+	/** Fire the MapMouseSupport method */
+	public void mouseExited(MouseEvent e) {
+		mouseSupport.fireMapMouseExited(e);
+	}
 
-    /**
-     * Fires the MapMouseSupport method.
-     * @param e mouse event.
-     */
-    public void mouseDragged( MouseEvent e ) {
-		mouseSupport.fireMapMouseDragged( e );
-    }
+	/** Fire the MapMouseSupport method */
+	public void mouseDragged(MouseEvent e) {
+		mouseSupport.fireMapMouseDragged(e);
+	}
 
-    /**
-     * Fires the MapMouseSupport method.
-     * @param e mouse event.
-     */
-    public void mouseMoved( MouseEvent e ) {
-		mouseSupport.fireMapMouseMoved( e );
-    }
-
+	/** Fire the MapMouseSupport method */
+	public void mouseMoved(MouseEvent e) {
+		mouseSupport.fireMapMouseMoved(e);
+	}
 }
