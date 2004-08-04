a318bd61c7e8228450318e8f6fbeae1fb9ac0a4e
Douglas Lau (doug.lau@dot.state.mn.us) <Douglas Lau (doug.lau@dot.state.mn.us)>
2004-08-04 18:59:23 +0000

    [project @ 2004-08-04 13:59:23 by lau1dou] Whitespace cleanup.

--- src/main/us/mn/state/dot/map/event/PanMouseMode.java
+++ src/main/us/mn/state/dot/map/event/PanMouseMode.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000  Minnesota Department of Transportation
+ * Copyright (C) 2000-2004  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,120 +16,95 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 package us.mn.state.dot.shape.event;
 
 import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
-
 import javax.swing.ImageIcon;
 import javax.swing.SwingUtilities;
-
 import us.mn.state.dot.shape.MapBean;
 
 /**
  *
  * @author <a href="mailto:erik.engstrom@dot.state.mn.us">Erik Engstrom</a>
- * @version $Revision: 1.3 $ $Date: 2003/05/06 20:58:15 $
  */
 public class PanMouseMode extends MouseModeAdapter {
 
    	/**
-     * Mouse Mode identifier, which is "Pan".
-     * This is returned on getID()
-     */
-    public final static transient String modeID = "Pan".intern();
+	 * Mouse Mode identifier, which is "Pan".
+	 * This is returned on getID()
+	 */
+	static public final transient String modeID = "Pan".intern();
 	
-	/** The start of a mouse drag*/
+	/** The start of a mouse drag */
 	private Point startPoint = null;
 
-    /**
+	/**
 	 * Creates new PanMouseMode
 	 * Default constructor.  Sets the ID to the modeID, and the
-     * consume mode to true. 
-     */
+	 * consume mode to true. 
+	 */
 	public PanMouseMode() {
-		this( true );
-    }
+		this(true);
+	}
     
 	/**
-     * Construct a PanMouseMode.
-     * The constructor that lets you set the consume mode. 
-     * @param consumeEvents the consume mode setting.
-     */
-    public PanMouseMode( boolean consumeEvents ){
-		super( modeID, consumeEvents );
-		cursor = Toolkit.getDefaultToolkit().createCustomCursor( new ImageIcon(
-			getClass().getResource( "/images/PanMod.gif" ) ).getImage(),
-			new Point( 0, 0 ), "Pan" );
-    }
+	 * Construct a PanMouseMode.
+	 * The constructor that lets you set the consume mode. 
+	 * @param consumeEvents the consume mode setting.
+	 */
+	public PanMouseMode(boolean consumeEvents) {
+		super(modeID, consumeEvents);
+		cursor = Toolkit.getDefaultToolkit().createCustomCursor(
+			new ImageIcon(getClass().getResource(
+			"/images/PanMod.gif")).getImage(),
+			new Point(0, 0), "Pan");
+	}
 
-    /**
-     * Fires the MapMouseSupport method.
-     * @param e mouse event.
-     */
-    public void mouseClicked( MouseEvent e ) {
-		mouseSupport.fireMapMouseClicked( e );
-    }
+	/** Fire the MapMouseSupport method */
+	public void mouseClicked(MouseEvent e) {
+		mouseSupport.fireMapMouseClicked(e);
+	}
 
-    /**
-     * Fires the MapMouseSupport method.
-     * @param e mouse event.
-     */
-    public void mousePressed( MouseEvent e ) {
+	/** Fire the MapMouseSupport method */
+	public void mousePressed(MouseEvent e) {
 		startPoint = e.getPoint();
-		mouseSupport.fireMapMousePressed( e );
-    }
+		mouseSupport.fireMapMousePressed(e);
+	}
 
-    /**
-     * Fires the MapMouseSupport method.
-     * @param e mouse event.
-     */
-    public void mouseReleased( MouseEvent e ) {
-		if ( SwingUtilities.isLeftMouseButton( e ) ){
-			MapBean map = ( MapBean ) e.getSource();
-			map.finishPan( startPoint, e.getPoint() );
+	/** Fire the MapMouseSupport method */
+	public void mouseReleased(MouseEvent e) {
+		if(SwingUtilities.isLeftMouseButton(e)) {
+			MapBean map = (MapBean)e.getSource();
+			map.finishPan(startPoint, e.getPoint());
 		}
-		mouseSupport.fireMapMouseReleased( e );
-    }
+		mouseSupport.fireMapMouseReleased(e);
+	}
 
-    /**
-     * Fires the MapMouseSupport method.
-     * @param e mouse event.
-     */
-    public void mouseEntered( MouseEvent e ) {
-		mouseSupport.fireMapMouseEntered( e );
-    }
+	/** Fire the MapMouseSupport method */
+	public void mouseEntered(MouseEvent e) {
+		mouseSupport.fireMapMouseEntered(e);
+	}
 
-    /**
-     * Fires the MapMouseSupport method.
-     * @param e mouse event.
-     */
-    public void mouseExited( MouseEvent e ) {
-		mouseSupport.fireMapMouseExited( e );
-    }
+	/** Fire the MapMouseSupport method */
+	public void mouseExited(MouseEvent e) {
+		mouseSupport.fireMapMouseExited(e);
+	}
 
-    /**
-     * Fires the MapMouseSupport method.
-     * @param e mouse event.
-     */
-    public void mouseDragged( MouseEvent e ) {
-		if ( SwingUtilities.isLeftMouseButton( e ) ){
-			MapBean map = ( MapBean ) e.getSource();
+	/** Fire the MapMouseSupport method */
+	public void mouseDragged(MouseEvent e) {
+		if(SwingUtilities.isLeftMouseButton(e)) {
+			MapBean map = (MapBean)e.getSource();
 			Point endPoint = e.getPoint();
-			map.pan( ( int ) ( endPoint.getX() - startPoint.getX() ), ( int ) (
-				endPoint.getY() - startPoint.getY() ) );
+			map.pan((int)(endPoint.getX() - startPoint.getX()),
+				(int)(endPoint.getY() - startPoint.getY()));
 		}
-		mouseSupport.fireMapMouseDragged( e );
-    }
+		mouseSupport.fireMapMouseDragged(e);
+	}
 
-    /**
-     * Fires the MapMouseSupport method.
-     * @param e mouse event.
-     */
-    public void mouseMoved( MouseEvent e ) {
-		mouseSupport.fireMapMouseMoved( e );
-    }
-
+	/** Fire the MapMouseSupport method */
+	public void mouseMoved(MouseEvent e) {
+		mouseSupport.fireMapMouseMoved(e);
+	}
 }
