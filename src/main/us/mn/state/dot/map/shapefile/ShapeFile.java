4c7f72082ea083958740c4dac9dca534e8600c36
Douglas Lau (doug.lau@dot.state.mn.us) <Douglas Lau (doug.lau@dot.state.mn.us)>
2004-08-05 02:54:17 +0000

    [project @ 2004-08-04 21:54:17 by lau1dou] Whitespace cleanup.

--- src/main/us/mn/state/dot/map/shapefile/ShapeFile.java
+++ src/main/us/mn/state/dot/map/shapefile/ShapeFile.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000  Minnesota Department of Transportation
+ * Copyright (C) 2000-2004  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,38 +27,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
-  * ShapeFile is an ESRI shape file reader.  It reads the file and creates an
-  * array of all the shapes and their coordinates.
-  *
-  * @author Douglas Lau
-  * @author <a href="mailto:erik.engstrom@dot.state.mn.us">Erik Engstrom</a>
-  * @version $Revision: 1.6 $ $Date: 2004/04/28 12:41:30 $
-  */
-public final class ShapeFile {
+ * ShapeFile is an ESRI shape file reader.  It reads the file and creates an
+ * array of all the shapes and their coordinates.
+ *
+ * @author Douglas Lau
+ * @author <a href="mailto:erik.engstrom@dot.state.mn.us">Erik Engstrom</a>
+ */
+public class ShapeFile {
 
-/**
-  *	ShapeFile header (100 bytes)
-  *
-  *	Position	Field				Value		Type				Size
-  *
-  *		0		File Code		9994		int (big)			4
-  *		4		Unused			0			int (big)			4
-  *		8		Unused			0			int (big)			4
-  *		12		Unused			0			int (big)			4
-  *		16		Unused			0			int (big)			4
-  *		20		Unused			0			int (big)			4
-  *		24		File Length					int (big)			4
-  *		28		Version			1000		int (little)		4
-  *		32		Shape Type					int (little)		4
-  *		36		Xmin							double (little)	8
-  *		44		Ymin							double (little)	8
-  *		52		Xmax							double (little)	8
-  *		60		Ymax							double (little)	8
-  *		68		Zmin				0.0		double (little)	8
-  *		76		Zmax				0.0		double (little)	8
-  *		84		Mmin				0.0		double (little)	8
-  *		92		Mmax				0.0		double (little)	8
-  */
+	/**
+	 *	ShapeFile header (100 bytes)
+	 *
+	 *	Pos	Field		Value	Type			Size
+	 *
+	 *	0	File Code	9994	int (big)		4
+	 *	4	Unused		0	int (big)		4
+	 *	8	Unused		0	int (big)		4
+	 *	12	Unused		0	int (big)		4
+	 *	16	Unused		0	int (big)		4
+	 *	20	Unused		0	int (big)		4
+	 *	24	File Length		int (big)		4
+	 *	28	Version		1000	int (little)		4
+	 *	32	Shape Type		int (little)		4
+	 *	36	Xmin			double (little)		8
+	 *	44	Ymin			double (little)		8
+	 *	52	Xmax			double (little)		8
+	 *	60	Ymax			double (little)		8
+	 *	68	Zmin		0.0	double (little)		8
+	 *	76	Zmax		0.0	double (little)		8
+	 *	84	Mmin		0.0	double (little)		8
+	 *	92	Mmax		0.0	double (little)		8
+	 */
 	private int shapeType;
 	private int version;
 	private double minX;
@@ -72,8 +71,8 @@ public final class ShapeFile {
 	public ShapeFile( URL url ) throws IOException {
 		ShapeDataInputStream in = new ShapeDataInputStream( url.openStream() );
 		String location = url.toExternalForm();
-		location = location.substring( 0, location.length() - 4 ) + ".dbf";
-		URL dbUrl = new URL( location );
+		location = location.substring(0, location.length() - 4) + ".dbf";
+		URL dbUrl = new URL(location);
 		DbaseInputStream dbaseStream = new DbaseInputStream( dbUrl );
 		readData( in, dbaseStream );
 	}
@@ -82,40 +81,41 @@ public final class ShapeFile {
 	public ShapeFile( String name ) throws IOException {
 		ShapeDataInputStream in = new ShapeDataInputStream(
 			new FileInputStream( name + ".shp" ) );
-		DbaseInputStream dbaseStream = new DbaseInputStream( name + ".dbf" );
-		readData( in, dbaseStream );
+		DbaseInputStream dbaseStream = new DbaseInputStream(
+			name + ".dbf");
+		readData(in, dbaseStream);
 	}
 
-	public void readData( ShapeDataInputStream shapeIn,
-			DbaseInputStream dbaseStream ) throws IOException {
-		shapeIn.skipBytes( 28 ); //start of header unused
+	public void readData(ShapeDataInputStream shapeIn,
+		DbaseInputStream dbaseStream) throws IOException
+	{
+		shapeIn.skipBytes(28); //start of header unused
 		version = shapeIn.readLittleInt();
 		shapeType = shapeIn.readLittleInt();
 		minX = shapeIn.readLittleDouble();
 		minY = shapeIn.readLittleDouble();
 		maxX = shapeIn.readLittleDouble();
 		maxY = shapeIn.readLittleDouble();
-		shapeIn.skipBytes( 32 ); //end of header unused
+		shapeIn.skipBytes(32); //end of header unused
 		try {
-			while( dbaseStream.hasNext() ) {
-				shapes.add( new ShapeObject( ShapeFactory.readShape(
-					shapeIn ), dbaseStream.nextRecord() ) );
+			while(dbaseStream.hasNext()) {
+				shapes.add(new ShapeObject(
+					ShapeFactory.readShape(shapeIn),
+					dbaseStream.nextRecord()));
 			}
-		} catch( EOFException e ) {
-			throw new IOException( "Shape file and Dbase file have different " +
-				" numbers of records." );
+		} catch(EOFException e) {
+			throw new IOException("Shape file and Dbase file " +
+				"have different numbers of records.");
 		}
 		dbaseStream.close();
 		shapeIn.close();
 	}
 
-	/**
-	 * Get the extent of the shape file.
-	 */
+	/** Get the extent of the shape file */
 	public Rectangle2D getExtent() {
 		double height = maxY - minY;
 		double width = maxX - minX;
-		return new Rectangle2D.Double( minX, minY, width, height );
+		return new Rectangle2D.Double(minX, minY, width, height);
 	}
 
 	public List getShapeList() {
@@ -131,5 +131,4 @@ public final class ShapeFile {
 	public int getShapeType() {
 		return shapeType;
 	}
-
 }
