c2083d6d884dc1c1205d983adadcaa27b74060ed
Douglas Lau (doug.lau@dot.state.mn.us) <Douglas Lau (doug.lau@dot.state.mn.us)>
2004-08-05 01:47:06 +0000

    [project @ 2004-08-04 20:47:06 by lau1dou] Whitespace cleanup. Dead code removed.

--- src/main/us/mn/state/dot/map/shapefile/ShapeRenderer.java
+++ src/main/us/mn/state/dot/map/shapefile/ShapeRenderer.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000  Minnesota Department of Transportation
+ * Copyright (C) 2000-2004  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,7 +16,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 package us.mn.state.dot.shape.shapefile;
 
 import us.mn.state.dot.shape.LayerRenderer;
@@ -24,98 +23,46 @@ import us.mn.state.dot.shape.MapObject;
 import us.mn.state.dot.shape.MapTip;
 import us.mn.state.dot.shape.Symbol;
 
-
 /**
  * Base class for all renderers used for shape files.
  *
  * @author <a href="mailto:erik.engstrom@dot.state.mn.us">Erik Engstrom</a>
- * @version $Revision: 1.4 $ $Date: 2003/05/06 20:58:15 $ 
  */
 public abstract class ShapeRenderer implements LayerRenderer {
-    
-	/** MapTip to display. */
-	MapTip mapTip = null;
-	
-	/** Symbol to paint with */
-	Symbol symbol = null;
-	
+
+	/** MapTip to display */
+	protected MapTip mapTip = null;
+
 	/** Name of renderer */
 	private String name = "";
-	
-	/**
-	 * Set the field for this renderer.
-	 */
-    public abstract void setField( String field );
-	
-	/**
-	 * Get the field used by this renderer.
-	 */
-    public abstract String getField();
-	
-	/**
-	 * Set the MapTip used by this renderer.
-	 */
-    public void setTip( MapTip m ) {
-        mapTip = m;
-    }
 
-	/**
-	 * Get the symbol used by this renderer.
-	 */
-    public Symbol getSymbol(){
-        return symbol;
-    }
+	/** Set the MapTip used by this renderer */
+	public void setTip(MapTip m) {
+		mapTip = m;
+	}
 
-	/**
-	 * Set the symbol used by this renderer.
-	 */
-    public void setSymbol( Symbol s ){
-        symbol = s;
-    }
-
-	/**
-	 * Get the Tip for the given map object.
-	 * @param layer, Layer that contains the map object.
-	 * @param i, index of the object.
-	 */
-    public String getTip( MapObject object ){
-        String result = null;
-        if ( mapTip != null ){
-            result = mapTip.getTip( object );
-        }
-    	return result;
-    }
-	
-	/**
-	 * Get the Symbols used by this renderer.
-	 */
-	public Symbol[] getSymbols() {
-		Symbol[] result = { symbol };
+	/** Get the tip for the given map object */
+	public String getTip(MapObject object) {
+		String result = null;
+		if(mapTip != null) {
+			result = mapTip.getTip(object);
+		}
 		return result;
 	}
-	
-	/**
-	 * Set the name of this renderer.
-	 */
-	public void setName( String s ) {
+
+	/** Set the name of this renderer */
+	public void setName(String s) {
 		name = s;
 	}
-	
-	/** 
-	 * Overrides Object.toString() 
-	 */
-	public final String toString(){
+
+	/** Overrides Object.toString() */
+	public String toString() {
 		String result = null;
-		if ( name == null ) {
+		if(name == null) {
 			result = super.toString();
 		} else {
 			result = name;
 		}
 		return result;
 	}
-	
-	public boolean isVisible( int index ) {
-		return true;
-	}
-	
 }
