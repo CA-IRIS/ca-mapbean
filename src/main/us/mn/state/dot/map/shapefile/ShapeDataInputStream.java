f623587c923a67fe03c74eaca279fbd1d7666156
Douglas Lau (doug.lau@dot.state.mn.us) <Douglas Lau (doug.lau@dot.state.mn.us)>
2004-08-05 02:45:56 +0000

    [project @ 2004-08-04 21:45:56 by lau1dou] Whitespace cleanup.

--- src/main/us/mn/state/dot/map/shapefile/ShapeDataInputStream.java
+++ src/main/us/mn/state/dot/map/shapefile/ShapeDataInputStream.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000  Minnesota Department of Transportation
+ * Copyright (C) 2000-2004  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,7 +16,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 package us.mn.state.dot.shape.shapefile;
 
 import java.io.DataInputStream;
@@ -25,58 +24,49 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
-  * Adds little endian double and integer reading to DataInputStream.
-  *
-  * @author <a href="mailto:erik.engstrom@dot.state.mn.us">Erik Engstrom</a>
-  * @version $Revision: 1.2 $ $Date: 2003/05/06 20:58:15 $ 
-  */
+ * Adds little endian double and integer reading to DataInputStream.
+ *
+ * @author <a href="mailto:erik.engstrom@dot.state.mn.us">Erik Engstrom</a>
+ */
 public class ShapeDataInputStream extends DataInputStream {
 
-	/** Creates new ShapeFileInputStream */
-    public ShapeDataInputStream( InputStream in ) {
-		super( in );
-    }
-	
-	/**
-	 * Read a little endian short.
-	 */
+	/** Create a new ShapeFileInputStream */
+	public ShapeDataInputStream(InputStream in) {
+		super(in);
+	}
+
+	/** Read a little endian short */
 	public short readLittleShort() throws EOFException, IOException {
 		short result = 0;
-		result += ( readByte() & 0xff ); 
-		result += ( readByte() & 0xff ) << 8;
+		result += (readByte() & 0xff);
+		result += (readByte() & 0xff) << 8;
 		return result;
 	}
-	
-	/**
-	 * Read a little endian integer.
-	 */
+
+	/** Read a little endian integer */
 	public int readLittleInt() throws EOFException, IOException {
 		int result = 0;
-		for ( int i = 0; i < 4; i++ ) {
-			result += ( ( readByte() & 0xff ) << ( i * 8 ) );
+		for(int i = 0; i < 4; i++) {
+			result += ((readByte() & 0xff) << (i * 8));
 		}
 		return result;
 	}
-	
-	/** 
-	 * Read a little endian double.
-	 */
+
+	/** Read a little endian double */
 	public double readLittleDouble() throws EOFException, IOException {
 		int first = readLittleInt();
 		int second = readLittleInt();
-		long temp = ( ( second & 0xFFFFFFFFL ) << 32 ) +
-			( first & 0xFFFFFFFFL );
-		return Double.longBitsToDouble( temp );
+		long temp = ((second & 0xFFFFFFFFL) << 32) +
+			(first & 0xFFFFFFFFL);
+		return Double.longBitsToDouble(temp);
 	}
-	
-	/**
-	 * Read a String.
-	 */
-	public String readString( int length ) throws EOFException, IOException {
-		byte[] buffer = new byte[ length ];
-		for ( int i = 0; i < length; i++ ) {
-			buffer[ i ] = readByte();
+
+	/** Read a string */
+	public String readString(int length) throws EOFException, IOException {
+		byte[] buffer = new byte[length];
+		for(int i = 0; i < length; i++) {
+			buffer[i] = readByte();
 		}
-		return new String( buffer ).trim();
+		return new String(buffer).trim();
 	}
 }
--- src/main/us/mn/state/dot/map/shapefile/ShapeFactory.java
+++ src/main/us/mn/state/dot/map/shapefile/ShapeFactory.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000  Minnesota Department of Transportation
+ * Copyright (C) 2000-2004  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,306 +24,197 @@ import java.awt.geom.PathIterator;
 import java.io.IOException;
 
 /**
-  * ShapeFactory reads shape data from a ShapeFileInputStream and creates 
-  * pathiterators.
-  *  The specification for ESRI(tm) shapefile can be found at 
-  * http://www.esri.com/library/whitepapers/pdfs/shapefile.pdf
-  *
-  * @author Douglas Lau
-  * @author <a href="mailto:erik.engstrom@dot.state.mn.us">Erik Engstrom</a>
-  * @version $Revision: 1.3 $ $Date: 2003/08/15 15:53:11 $ 
-  */
-public final class ShapeFactory {
-	
+ * ShapeFactory reads shape data from a ShapeFileInputStream and creates
+ * pathiterators.
+ *  The specification for ESRI(tm) shapefile can be found at
+ * http://www.esri.com/library/whitepapers/pdfs/shapefile.pdf
+ *
+ * @author Douglas Lau
+ * @author <a href="mailto:erik.engstrom@dot.state.mn.us">Erik Engstrom</a>
+ */
+public class ShapeFactory {
+
 	/** Shape Type Constants */
 	public static final int NULL = 0;
-	
+
 	public static final int POINT = 1;
-	
+
 	public static final int POLYLINE = 3;
-	
+
 	public static final int POLYGON = 5;
-	
+
 	public static final int POINTZ = 11;
 
 	/** Create a Shape and return a PathIterator for it */
-	static public Shape readShape( ShapeDataInputStream in ) 
-			throws IOException {
-		int skipped = in.skipBytes( 8 );
+	static public Shape readShape(ShapeDataInputStream in)
+		throws IOException
+	{
+		int skipped = in.skipBytes(8);
 		int type = in.readLittleInt();
-		GeneralPath path = new GeneralPath( GeneralPath.WIND_EVEN_ODD );
-		switch( type ) {
+		GeneralPath path = new GeneralPath(GeneralPath.WIND_EVEN_ODD);
+		switch(type) {
 			case NULL:
-				path.append( new Null(), false );
+				path.append(new Null(), false);
 				return path;
 			case POINT:
-				path.append( new Point( in ), false );
+				path.append(new Point(in), false);
 				return path;
 			case POLYLINE:
-				path.append( new PolyLine( in ), false );
+				path.append(new PolyLine(in), false);
 				return path;
 			case POLYGON:
-				path.append( new Polygon( in ), false );
+				path.append(new Polygon(in), false);
 				return path;
 			case POINTZ:
-				path.append( new PointZ( in ), false );
+				path.append(new PointZ(in), false);
 				return path;
 			default:
-				System.err.println( " Invalid shape type: " + type );
-				System.err.println( " " + skipped + " bytes were skipped." );
-				throw new IOException( "Invalid shape type" );
+				throw new IOException("Invalid shape type: " +
+					type);
 		}
 	}
 
-    private static Shape readNull( ) {
-        return new GeneralPath( GeneralPath.WIND_EVEN_ODD );
-    }
-
-    private static Shape readPoint( ShapeDataInputStream in )
-			throws IOException {
-        GeneralPath result = new GeneralPath( GeneralPath.WIND_EVEN_ODD );
-        result.moveTo( ( float ) in.readLittleDouble(),
-			( float ) in.readLittleDouble() );
-        return result;
-    }
-
-
-    public static final class PolyHeader {
-		
-		private final int numParts;
-		private final int numPoints;
-		private final int[] parts;
-		
-        public PolyHeader( ShapeDataInputStream in ) throws IOException {
-			in.skipBytes( 32 );
-			numParts = in.readLittleInt();
-			numPoints = in.readLittleInt();
-			parts = new int[ numParts ];
-			for ( int i = 0; i < numParts; i++ ) {
-				parts[ i ] = in.readLittleInt();
-			}
-        }
-		
-		public int getPartCount() {
-			return numParts;
-		}
-		
-		public int getPointCount() {
-			return numPoints;
-		}
-		
-		public int[] getParts() {
-			return parts;
-		}
-        
-    }
-    
-    private static Shape readPolyLine( ShapeDataInputStream in )
-			throws IOException  {
-		PolyHeader header = new PolyHeader( in );	
-        int parts[] = header.getParts();
-		int numPoints = header.getPointCount();
-		int numParts = header.getPartCount() - 1;
-		GeneralPath result = new GeneralPath( GeneralPath.WIND_EVEN_ODD,
-			numPoints );
-		int part = 0;
-		for ( int i = 0; i < numPoints; i++ ) {
-			if ( i == parts[ part ] ) {
-				result.moveTo( ( float ) in.readLittleDouble(),
-					( float ) in.readLittleDouble() );
-				if ( part < numParts ) {
-					part++;
-				}
-			} else {
-				result.lineTo( ( float ) in.readLittleDouble(),
-				( float ) in.readLittleDouble() );
-			}
-        }
-        return result;
-    }
-
-    private static Shape readPolygon( ShapeDataInputStream in ) 
-			throws IOException {
-        PolyHeader header = new PolyHeader( in );	
-        int parts[] = header.getParts();
-		int numPoints = header.getPointCount();
-		int numParts = header.getPartCount() - 1;
-		GeneralPath result = new GeneralPath( GeneralPath.WIND_EVEN_ODD,
-			numPoints );
-		int part = 0;
-		int nextPart = parts[ part ];
-		for ( int i = 0; i < numPoints; i++ ) {
-			int nextPoint = i + 1;
-			if ( i == parts[ part ] ) {
-				result.moveTo( ( float ) in.readLittleDouble(),
-					( float ) in.readLittleDouble() );
-				if ( part < numParts ) {
-					part++;
-					nextPart = parts[ part ];
-				}
-			} else if ( nextPoint == nextPart || nextPoint == numPoints ) {
-				result.closePath();
-			} else {
-				result.lineTo( ( float ) in.readLittleDouble(), 
-					( float ) in.readLittleDouble() );
-			}
-        }
-        return result;
-    }
-
-    private static Shape readPointZ( ShapeDataInputStream in ) 
-			throws IOException {
-        GeneralPath result = new GeneralPath( GeneralPath.WIND_EVEN_ODD );
-        result.moveTo( ( float ) in.readLittleDouble(), ( float ) 
-			in.readLittleDouble() );
-        in.skipBytes( 16 );
-        return result;
-    }
-
 	/** Null shape */
-	static public final class Null implements PathIterator {
-		
-		public Null() {
-		}
-		
-		public int currentSegment( double [] coords ) {
+	static public class Null implements PathIterator {
+
+		public int currentSegment(double [] coords) {
 			return SEG_MOVETO;
 		}
-		
-		public int currentSegment( float [] coords ) {
+
+		public int currentSegment(float [] coords) {
 			return SEG_MOVETO;
 		}
-		
+
 		public void next() {}
-		
+
 		public boolean isDone() {
-			return true; 
+			return true;
 		}
-		
+
 		public int getWindingRule() {
-			return WIND_EVEN_ODD; 
+			return WIND_EVEN_ODD;
 		}
 	}
 
 	/**
-	  * Point shape structure
-	  *
-	  * Position	Field			Type				Size
-	  *	0			Shape Type	int (little)		4
-	  *	4			X				double (little)	8
-	  *	12			Y				double (little)	8
-	  */
-	static public final class Point implements PathIterator {
-		
+	 * Point shape structure
+	 *
+	 * Pos	Field		Type		Size
+	 * 0	Shape Type	int (little)	4
+	 * 4	X		double (little)	8
+	 * 12	Y		double (little)	8
+	 */
+	static public class Point implements PathIterator {
+
 		private final double X;
 		private final double Y;
 		private boolean done = false;
-		
-		public Point( ShapeDataInputStream in ) throws IOException {
+
+		public Point(ShapeDataInputStream in) throws IOException {
 			X = in.readLittleDouble();
 			Y = in.readLittleDouble();
 		}
-		
-		public Point( double x, double y ) {
+
+		public Point(double x, double y) {
 			X = x;
 			Y = y;
 		}
-		
-		public int currentSegment( double [] coords ) {
-			coords[ 0 ] = X;
-			coords[ 1 ] = Y;
+
+		public int currentSegment(double [] coords) {
+			coords[0] = X;
+			coords[1] = Y;
 			return SEG_MOVETO;
 		}
-		
-		public int currentSegment( float [] coords ) {
-			coords[ 0 ] = ( float ) X;
-			coords[ 1 ] = ( float ) Y;
+
+		public int currentSegment(float [] coords) {
+			coords[0] = (float)X;
+			coords[1] = (float)Y;
 			return SEG_MOVETO;
 		}
-		
+
 		public void next() {
-			done = true; 
+			done = true;
 		}
-		
-		public final boolean isDone() {
-			return done; 
+
+		public boolean isDone() {
+			return done;
 		}
-		
+
 		public final int getWindingRule() {
-			return WIND_EVEN_ODD; 
+			return WIND_EVEN_ODD;
 		}
 	}
-		
+
 	/**
-	  * Point shape structure
-	  *
-	  * Position	Field			Type				Size
-	  *	0			Shape Type	int (little)		4
-	  *	4			X				double (little)	8
-	  *	12			Y				double (little)	8
-	  * 20          z               double (little) 8
-	  */
-	static public final class PointZ implements PathIterator {
-		
+	 * Point shape structure
+	 *
+	 * Pos	Field		Type			Size
+	 * 0	Shape Type	int (little)		4
+	 * 4	X		double (little)		8
+	 * 12	Y		double (little)		8
+	 * 20	Z		double (little)		8
+	 */
+	static public class PointZ implements PathIterator {
+
 		private final double X;
 		private final double Y;
 		private final double Z;
 		private final double measure;
 		private boolean done = false;
-		
-		public PointZ( ShapeDataInputStream in ) throws IOException {
+
+		public PointZ(ShapeDataInputStream in) throws IOException {
 			X = in.readLittleDouble();
 			Y = in.readLittleDouble();
 			Z = in.readLittleDouble();
 			measure = in.readLittleDouble();
 		}
-		
-		public PointZ( double x, double y, double z, double measure ) {
+
+		public PointZ(double x, double y, double z, double measure) {
 			X = x;
 			Y = y;
 			Z = z;
 			this.measure = measure;
 		}
-		
-		public int currentSegment( double [] coords ) {
-			coords[ 0 ] = X;
-			coords[ 1 ] = Y;
+
+		public int currentSegment(double [] coords) {
+			coords[0] = X;
+			coords[1] = Y;
 			return SEG_MOVETO;
 		}
-		
-		public int currentSegment( float [] coords ) {
-			coords[ 0 ] = ( float ) X;
-			coords[ 1 ] = ( float ) Y;
+
+		public int currentSegment(float [] coords) {
+			coords[0] = (float)X;
+			coords[1] = (float)Y;
 			return SEG_MOVETO;
 		}
-		
+
 		public void next() {
-			done = true; 
+			done = true;
 		}
-		
-		public final boolean isDone() {
-			return done; 
+
+		public boolean isDone() {
+			return done;
 		}
-		
-		public final int getWindingRule() {
-			return WIND_EVEN_ODD; 
+
+		public int getWindingRule() {
+			return WIND_EVEN_ODD;
 		}
 	}
 
 	/**
-	  * PolyLine / Polygon shape structure
-	  *
-	  * Position	Field			Type				Size
-	  *	0			Shape Type	int (little)		4
-	  *	4			Xmin			double (little)	8
-	  *	12			Ymin			double (little)	8
-	  *	20			Xmax			double (little)	8
-	  *	28			Ymax			double (little)	8
-	  *	36			NumParts		int (little)		4
-	  *	40			NumPoints	int (little)		4
-	  *	44			Parts			int (little)		4 * NumParts
-	  *	X			Points		point					16 * NumPoints
-	  */
+	 * PolyLine / Polygon shape structure
+	 *
+	 * Pos	Field		Type			Size
+	 * 0	Shape Type	int (little)		4
+	 * 4	Xmin		double (little)		8
+	 * 12	Ymin		double (little)		8
+	 * 20	Xmax		double (little)		8
+	 * 28	Ymax		double (little)		8
+	 * 36	NumParts	int (little)		4
+	 * 40	NumPoints	int (little)		4
+	 * 44	Parts		int (little)		4 * NumParts
+	 * X	Points		point			16 * NumPoints
+	 */
 	static public abstract class Poly implements PathIterator {
 		protected int numParts;
 		protected int numPoints;
@@ -332,73 +223,67 @@ public final class ShapeFactory {
 		protected double[] ypoints;
 		protected int[] parts;
 
-		/** Private constructor */
-		public Poly( ShapeDataInputStream in ) throws IOException {
-			in.skipBytes( 32 );
+		public Poly(ShapeDataInputStream in) throws IOException {
+			in.skipBytes(32);
 			numParts = in.readLittleInt();
 			numPoints = in.readLittleInt();
-			xpoints = new double[ numPoints ];
-			ypoints = new double[ numPoints ];
-			parts = new int[ numParts ];
+			xpoints = new double[numPoints];
+			ypoints = new double[numPoints];
+			parts = new int[numParts];
 			int i;
-			for( i = 0; i < numParts; i++ ) {
-				parts[ i ] = in.readLittleInt();
+			for(i = 0; i < numParts; i++) {
+				parts[i] = in.readLittleInt();
 			}
-			for( i = 0; i < numPoints; i++ ) {
-				xpoints[ i ] = in.readLittleDouble();
-				ypoints[ i ] = in.readLittleDouble();
+			for(i = 0; i < numPoints; i++) {
+				xpoints[i] = in.readLittleDouble();
+				ypoints[i] = in.readLittleDouble();
+			}
+		}
 
-			}
+		protected void fillCurrentSegment(double [] coords) {
+			coords[0] = xpoints[point];
+			coords[1] = ypoints[point];
 		}
-		
-		protected final void fillCurrentSegment( double [] coords ) {
-			coords[ 0 ] = xpoints[ point ];
-			coords[ 1 ] = ypoints[ point ];
+
+		protected void fillCurrentSegment( float [] coords ) {
+			coords[0] = (float)xpoints[point];
+			coords[1] = (float)ypoints[point];
 		}
-		
-		protected final void fillCurrentSegment( float [] coords ) {
-			coords[ 0 ] = ( float ) xpoints[ point ];
-			coords[ 1 ] = ( float ) ypoints[ point ];
+
+		public void next() {
+			point++;
 		}
-		
-		public final void next() {
-			point++; 
-		}
-		
-		public final boolean isDone() {
-			if ( point < numPoints ) {
-				return false;
-			}
+
+		public boolean isDone() {
+			if(point < numPoints) return false;
 			return true;
 		}
-		
-		public final int getWindingRule() {
-			return WIND_EVEN_ODD; 
+
+		public int getWindingRule() {
+			return WIND_EVEN_ODD;
 		}
 	}
 
-
 	/** PolyLine shape structure */
-	static public final class PolyLine extends Poly {
+	static public class PolyLine extends Poly {
 
-		/** Private constructor */
-		public PolyLine( ShapeDataInputStream in ) throws IOException {
-			super( in );
+		public PolyLine(ShapeDataInputStream in) throws IOException {
+			super(in);
 		}
-		
-		public int currentSegment( double [] coords ) {
-			super.fillCurrentSegment( coords );
+
+		public int currentSegment(double [] coords) {
+			super.fillCurrentSegment(coords);
 			return currentSegmentType();
 		}
-		
-		public int currentSegment( float [] coords ) {
-			super.fillCurrentSegment( coords );
+
+		public int currentSegment(float [] coords) {
+			super.fillCurrentSegment(coords);
 			return currentSegmentType();
 		}
-		
+
 		private int currentSegmentType() {
-			for ( int i = 0; i < numParts; i++ ) {
-				if ( point == parts[ i ] ) {
+			for(int i = 0; i < numParts; i++) {
+				if(point == parts[i]) {
 					return SEG_MOVETO;
 				}
 			}
@@ -406,36 +291,34 @@ public final class ShapeFactory {
 		}
 	}
 
-
 	/** Polygon shape structure */
-	static public final class Polygon extends Poly {
+	static public class Polygon extends Poly {
 
-		/** Private constructor */
-		public Polygon( ShapeDataInputStream in ) throws IOException {
-			super( in );
+		public Polygon(ShapeDataInputStream in) throws IOException {
+			super(in);
 		}
-		
-		public int currentSegment( double [] coords ) {
-			super.fillCurrentSegment( coords );
+
+		public int currentSegment(double [] coords) {
+			super.fillCurrentSegment(coords);
 			return currentSegmentType();
 		}
-		
-		public int currentSegment( float [] coords ) {
-			super.fillCurrentSegment( coords );
+
+		public int currentSegment(float [] coords) {
+			super.fillCurrentSegment(coords);
 			return currentSegmentType();
 		}
-		
+
 		private int currentSegmentType() {
 			int nextPoint = point + 1;
-			for ( int i = 0; i < numParts; i++ ) {
-				int part = parts[ i ];
-				if ( point == part ) {
+			for(int i = 0; i < numParts; i++) {
+				int part = parts[i];
+				if(point == part) {
 					return SEG_MOVETO;
-				} else if( nextPoint == part ) {
+				} else if(nextPoint == part) {
 					return SEG_CLOSE;
 				}
 			}
-			if ( nextPoint == numPoints ) {
+			if(nextPoint == numPoints) {
 				return SEG_CLOSE;
 			}
 			return SEG_LINETO;
