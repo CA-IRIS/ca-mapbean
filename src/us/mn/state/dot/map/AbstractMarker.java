dff292a2a539842a28b76362bb04bcec6dd10ed0
Douglas Lau (doug.lau@state.mn.us) <Douglas Lau (doug.lau@state.mn.us)>
2010-12-20 10:16:51 -0600

    Got rid of .marker subpackage.
    --HG--
    rename : src/us/mn/state/dot/map/marker/AbstractMarker.java => src/us/mn/state/dot/map/AbstractMarker.java

--- src/us/mn/state/dot/map/AbstractMarker.java
+++ src/us/mn/state/dot/map/AbstractMarker.java
@@ -12,7 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-package us.mn.state.dot.map.marker;
+package us.mn.state.dot.map;
 
 import java.awt.Rectangle;
 import java.awt.Shape;
